68cc235b9658942b33c83f9c1d4c01fdd75a78a8
hritikch24 <hc@rapidinnovation.dev>
2022-07-19 18:27:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+const apm = require('elastic-apm-node').start({
+  // Override service name from package.json
+  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+  serviceName: '',
+
+  // Use if APM Server requires a token
+  secretToken: '',
+
+  // Use if APM Server uses API keys for authentication
+  apiKey: '',
+
+  // Set custom APM Server URL (default: http://localhost:8200)
+  serverUrl: '',
+})
 var http = require('http')
 
 var port = 4000
